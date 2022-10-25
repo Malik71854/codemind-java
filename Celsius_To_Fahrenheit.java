cc2a545a71233583ee3fb35d76c674d97546235c
Malik71854 <85397133+Malik71854@users.noreply.github.com>
2022-10-25 20:17:03 +0530

    import java.util.*; import java.lang.Math; class main {     public static void main(String args[])     {        // double n,r,a;         //n=3.14;         int n,m,l;
            double s,a,b;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
             
            a=(float)n*1.8;
             s=(float)32+a;
            
            System.out.format("%.2f",s);
        }
    }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.Math;
+class main
+{
+    public static void main(String args[])
+    {
+       // double n,r,a;
+        //n=3.14;
+        int n,m,l;
+    
+        double s,a,b;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+         
+        a=(float)n*1.8;
+         s=(float)32+a;
+        
+        System.out.format("%.2f",s);
+    }
+}
