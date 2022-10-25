54263671fc90bd1da4beb396c59af7eba125bc10
Malik71854 <85397133+Malik71854@users.noreply.github.com>
2022-10-25 15:25:16 +0530

    import java.util.*; class main {     public static void main(String args[])     {         int t;         Scanner sc=new Scanner(System.in);         t=sc.nextInt();         int s=0;         for(int i=1;i<=t;i++)         {             s=s+i;         }         System.out.print(s);
        }
    }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        int t;
+        Scanner sc=new Scanner(System.in);
+        t=sc.nextInt();
+        int s=0;
+        for(int i=1;i<=t;i++)
+        {
+            s=s+i;
+        }
+        System.out.print(s);
+        
+    }
+}
